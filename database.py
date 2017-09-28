dfbfcb19802433e91c1e1a722b54b471d8ba9cba
ramblersmith <robert.amblersmith@gmail.com>
2017-09-28 12:45:10 +0200

    Update from Rob's laptop

--- database.py
+++ database.py
@@ -1,9 +1,11 @@
 
 import sqlite3
 
+def db_exists(db_name):
+    # check if database exists, and if not create it
 
 def write_historic_data(exchange_prices):
-    conn = sqlite3.connect('crypto.db')
+    conn = sqlite3.connect('db/crypto.db')
     c = conn.cursor()
     # Create table Historic_Price
     c.execute("CREATE TABLE Historic_Price (Timestamp text, Exchange text, Bid_Price real, Bid_Volume real, Ask_Price real, Ask_Volume)")
--- main.py
+++ main.py
@@ -1,16 +1,54 @@
 import krakenex
-
+import json
 import pprint
+import numpy as np
 
+if __name__ == "__main__":
+    print("main")
+
+
+'''
+def get_kraken_data(self):
+
+    print("kraken fucntion")
+
+'''
 k = krakenex.API()
+records = 6
 
-response = k.query_public('Depth', {'pair': 'XXBTZUSD', 'count': '10'})
-pprint.pprint(response)
+response = k.query_public('Depth', {'pair': 'XXBTZUSD', 'count': records})
+# pprint.pprint(response)
+
+data = json.dumps(response)
+a = json.loads(data)
+kraken_arr = np.array([])
+tstamp = "now"
+temp_arr = []
+
+i = 0
+while i < records:
+    np.append(kraken_arr, i)
+    temp_arr = (tstamp, "Kraken", a["result"]["XXBTZUSD"]["bids"][i][0], a["result"]["XXBTZUSD"]["bids"][i][1],
+                      a["result"]["XXBTZUSD"]["asks"][i][0], a["result"]["XXBTZUSD"]["asks"][i][1])
+    np.append(kraken_arr, temp_arr, axis=0)
+    i += 1
+
+pprint.pprint(kraken_arr[0])
+# print(kraken_arr)
+
+'''for k in a["result"]["XXBTZUSD"]:
+    pprint.pprint("testing 123... ")
+    print(k)
+    pprint.pprint(a["result"]["XXBTZUSD"]["bids"][0])
+    # pprint.pprint(k["bids"])
+'''
+
 
 # Get the data into an array and write to DB
 
-# Get the exchange prices every minute
 
+# Get the exchange prices every minute
+'''
 import sched, time
 s = sched.scheduler(time.time, time.sleep)
 def do_something(sc):
@@ -20,3 +58,5 @@ def do_something(sc):
 
 s.enter(60, 1, do_something, (s,))
 s.run()
+
+'''
